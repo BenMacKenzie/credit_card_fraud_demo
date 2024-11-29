c250384ea52e56693bed6ff13a757e011b1958aa
benmackenzie <benmackenzie>
2024-11-29 17:24:32 +0000

    udpates

--- Build Model.py
+++ Build Model.py
@@ -1,4 +1,13 @@
 # Databricks notebook source
+pip install databricks-feature-store mlflow scikit-learn
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
 catalog = 'bmac'
 schema = 'credit_card_fraud_demo'
 
@@ -14,7 +23,12 @@ spark.sql(f"use {catalog}.{schema}")
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ###Let's start by looking out our data.  The first table is an scd type 2 dimension table
+# MAGIC ## Data Exploration
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Customers:  scd type 2 dimension table
 
 # COMMAND ----------
 
@@ -24,7 +38,7 @@ spark.sql(f"use {catalog}.{schema}")
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ###The next table is a type 1 dim table
+# MAGIC ###Mercants: Type 1 SCD table
 
 # COMMAND ----------
 
@@ -54,7 +68,9 @@ spark.sql(f"use {catalog}.{schema}")
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ###We want to calculate the distance between the merchant and the customers home address.  We cannot model this as a feature because we don't know the merchant until inference time.  So we model it as a function.
+# MAGIC ### Define Function to calcuate distance between POS and customer home address
+# MAGIC
+# MAGIC We want to calculate the distance between the merchant and the customers home address.  We cannot model this as a feature because we don't know the merchant until inference time.  So we model it as a function.
 
 # COMMAND ----------
 
@@ -74,15 +90,13 @@ spark.sql(f"use {catalog}.{schema}")
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ###Features:
-# MAGIC 1. transaction amount
-# MAGIC 2. aggregates related to spend over time windows
-# MAGIC 3. distance
+# MAGIC ## Build Training Data Set
+# MAGIC
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ###Let define our 'EOL' table 
+# MAGIC ###Define  'EOL' table 
 # MAGIC 1. pk for the entity: transaction_id
 # MAGIC 2. observation date: transaction_date
 # MAGIC 3. fk for customer: primary account number
@@ -101,6 +115,11 @@ spark.sql(f"use {catalog}.{schema}")
 from databricks.feature_engineering.entities.feature_lookup import FeatureLookup
 from databricks.feature_engineering import FeatureEngineeringClient, FeatureFunction
 fe = FeatureEngineeringClient()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Feature Lookups
 
 # COMMAND ----------
 
@@ -138,6 +157,11 @@ feature_lookups = [
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ###Build Training Dataset
+
+# COMMAND ----------
+
 eol_df = spark.sql('select * from fraud_eol')
 
 training_set = fe.create_training_set(
@@ -154,6 +178,16 @@ display(training_df)
 
 # COMMAND ----------
 
+training_df = training_df.toPandas()
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Build Model
+
+# COMMAND ----------
+
 import mlflow
 import sklearn
 from sklearn import set_config
@@ -166,119 +200,47 @@ from sklearn.tree import DecisionTreeClassifier
 from pyspark.sql.functions import col
 from mlflow.models.signature import infer_signature
 
-mlflow.set_registry_uri("databricks-uc")
+
 mlflow.sklearn.autolog(log_input_examples=True,silent=True)
 
 
-skdtc_classifier = DecisionTreeClassifier(
-  criterion="gini",
-  max_depth=10
-)
 
-model = Pipeline([
-    ("classifier", skdtc_classifier),
-])
+with mlflow.start_run() as run:
+  
+  skdtc_classifier = DecisionTreeClassifier(
+    criterion="gini",
+    max_depth=5
+  )
 
-training_df = training_df.toPandas()
-X_train = training_df.drop(['label'], axis=1)
-y_train = training_df['label'] = training_df['label'].astype(float)
+  model = Pipeline([
+      ("classifier", skdtc_classifier),
+  ])
 
-x_sample = X_train[0:2]
-y_sample = y_train[0:2]
+ 
+  X_train = training_df.drop(['label'], axis=1)
+  y_train = training_df['label'] = training_df['label'].astype(float)
 
-with mlflow.start_run():
+  x_sample = X_train[0:2]
+  y_sample = y_train[0:2]
   model.fit(X_train, y_train)
-  training_set=training_set, 
   input_example=X_train[0:2]
-  signature=infer_signature(x_sample, y_sample), # schema of the dataset, not necessary with FS, 
 
+  fe.log_model(
+    model=model,
+    artifact_path="model",
+    flavor=mlflow.sklearn,
+    training_set=training_set,
+    input_example=x_sample,
+  )
 
-
-
+  
 
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Let use the model for Inference
 # MAGIC
-# MAGIC Note that I need to supply all of the data.  The model stored in mflow is just a regular sklearn model.  There is nothing to connect it to the feature store.
-
-# COMMAND ----------
-
-import pandas as pd
-
-logged_model = 'runs:/5e024a5f097a4bfdb016b15e434d2fe6/model'
-
-# Load model as a PyFuncModel.
-loaded_model = mlflow.pyfunc.load_model(logged_model)
-
-data = {
-    "columns": [
-        "primary_account_number",
-        "amount",
-        "14_day_total",
-        "7_day_total",
-        "7_day_transaction_count",
-        "merchant_distance_from_home"
-    ],
-    "data": [
-        [10000, 132.12, 1564.62, 386.88, 1, 240],
-        [10001, 78.11, 2752.53, 1574.79, 2, 97],
-        [10002, 475.33, 2142.27, 1755.39, 2, 230],
-        [10003, 419.24, 3805.8, 2231.01, 3, 47],
-        [10004, 286.99, 3135.03, 1711.86, 3, 135]
-    ]
-}
-
-# Create the DataFrame
-df = pd.DataFrame(data['data'], columns=data['columns']).astype({
-    'primary_account_number': 'int64',
-    'amount': 'float32',
-    '14_day_total': 'float64',
-    '7_day_total': 'float64',
-    '7_day_transaction_count': 'int64',
-    'merchant_distance_from_home': 'float64'
-})
-
-
-loaded_model.predict(pd.DataFrame(df))
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ### Let's make the connection between the model and the feature store by using the fe.log_model api call
-# MAGIC
-# MAGIC note that this will create a new run in mlflow
-
-# COMMAND ----------
-
-run_id = '5e024a5f097a4bfdb016b15e434d2fe6'
-logged_modeld_model = f"runs:/{run_id}/model"
-loaded_model = mlflow.pyfunc.load_model(logged_model)
-
-x_sample = eol_df.toPandas()[0:3]
-
-fe.log_model(
-              model=loaded_model, # object of your model
-              artifact_path="model",
-              flavor=mlflow.sklearn, 
-              training_set=training_set, 
-              input_example=x_sample, # example of the dataset, should be Pandas 
-              registered_model_name=f"{catalog}.{schema}.transaction_fraud"
-          )
-
-metrics = mlflow.get_run(run_id).data.metrics
-params = mlflow.get_run(run_id).data.params
-mlflow.log_metrics(metrics)
-mlflow.log_params(params)
-mlflow.set_tag(key='feature_store', value='churn_model_demo')
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
-# MAGIC ## Let's use the model for inference
+# MAGIC ##Use the model for inference
 
 # COMMAND ----------
 
@@ -287,13 +249,42 @@ display(inference_df)
 
 # COMMAND ----------
 
-from mlflow import MlflowClient
-client = MlflowClient()
-client.set_registered_model_alias(f"{catalog}.{schema}.transaction_fraud", "champion", 7)
+from databricks.feature_store import FeatureStoreClient
+
+fs = FeatureStoreClient()
+
+logged_model = 'runs:/1be30fca7e914f3ebc28f0ca61f98fe9/model'
+#logged_model = f"runs:/{run.info.run_id}/model"
+
+# This model was packaged by Feature Store.
+# To retrieve features prior to scoring, call FeatureStoreClient.score_batch.
+predictions = fs.score_batch(logged_model, inference_df)
+display(predictions)
 
 # COMMAND ----------
 
-#note that the URI is made up of current catalog and schema and the name of the model from fs.log_model above. 
+# MAGIC %md
+# MAGIC ## Register With Unity Catalog
+
+# COMMAND ----------
+
+
+logged_model = 'runs:/1be30fca7e914f3ebc28f0ca61f98fe9/model'
+#logged_model = f"runs:/{run.info.run_id}/model"
+
+mlflow.register_model(
+  logged_model, f"{catalog}.{schema}.transaction_fraud"
+)
+
+# COMMAND ----------
+
+from mlflow import MlflowClient
+client = MlflowClient()
+client.set_registered_model_alias(f"{catalog}.{schema}.transaction_fraud", "champion", 9)
+
+# COMMAND ----------
+
+
 
 model_version_uri = f"models:/{catalog}.{schema}.transaction_fraud@champion" 
 predictions = fe.score_batch(
@@ -301,6 +292,7 @@ predictions = fe.score_batch(
     df=inference_df
 )
 display(predictions)
+
 
 # COMMAND ----------
 
@@ -340,13 +332,17 @@ def online_table_exists(table_name):
         print(str(e))
         return 'already exists' in str(e)
     return False
-
-def create_online_table(table_name, primary_key_columns, timeseries_key=None):
-    if not online_table_exists(table_name):
+def create_online_table(table_name, pks, timeseries_key=None):
+    w = WorkspaceClient()
+    online_table_name = table_name+"_online"
+    if not online_table_exists(online_table_name):
+        from databricks.sdk.service import catalog as c
+        print(f"Creating online table for {online_table_name}...")
         spark.sql(f'ALTER TABLE {table_name} SET TBLPROPERTIES (delta.enableChangeDataFeed = true)')
-        online_table_name = table_name+"_online"
-        spec = c.OnlineTableSpec(source_table_full_name=table_name, primary_key_columns=primary_key_columns, run_triggered={'triggered': 'true'}, timeseries_key=timeseries_key)
+        spec = c.OnlineTableSpec(source_table_full_name=table_name, primary_key_columns=pks, run_triggered={'triggered': 'true'}, timeseries_key=timeseries_key)
         w.online_tables.create(name=online_table_name, spec=spec)
+
+
 
 
 create_online_table(f"{catalog}.{schema}.transaction_aggregates", ["customer_id"], "observation_date") 
@@ -358,7 +354,7 @@ create_online_table(f"{catalog}.{schema}.customers", ["customer_id"], "observati
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Then deploy the model to a serving endpoint
+# MAGIC ### Deploy the model to a serving endpoint
 # MAGIC
 # MAGIC ####serve model using Unity Catalog UI
 # MAGIC
@@ -374,9 +370,11 @@ create_online_table(f"{catalog}.{schema}.customers", ["customer_id"], "observati
 # COMMAND ----------
 
 
+from databricks.sdk.service.serving import ServedModelInput, ServedModelInputWorkloadSize, EndpointCoreConfigInput
+
 endpoint_name = "bmac_cc_fraud_endpoint"
 wc = WorkspaceClient()
-served_models =[ServedModelInput(model_full_name, model_version=latest_model.version, workload_size=ServedModelInputWorkloadSize.SMALL, scale_to_zero_enabled=True)]
+served_models =[ServedModelInput(f"{catalog}.{schema}.transaction_fraud", model_version=9, workload_size=ServedModelInputWorkloadSize.SMALL, scale_to_zero_enabled=True)]
 try:
     print(f'Creating endpoint {endpoint_name} with latest version...')
     wc.serving_endpoints.create_and_wait(endpoint_name, config=EndpointCoreConfigInput(served_models=served_models))
@@ -399,10 +397,26 @@ except Exception as e:
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Call model
+# MAGIC ### Call model
 
 # COMMAND ----------
 
+data = [
+    {
+        "primary_account_number": 10048,
+        "observation_date": "2024-04-15",
+        "merchant_id": 20018,
+        "amount": 465.25
+    }
+]
+print('Data sent to the model:')
+print(data)
+
+
+inferences = wc.serving_endpoints.query(endpoint_name, inputs=data)
+print(inferences.predictions)
+
+# COMMAND ----------
 
 import os
 import requests
@@ -414,7 +428,7 @@ def create_tf_serving_json(data):
     return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
 
 def score_model(dataset):
-    url = 'https://adb-984752964297111.11.azuredatabricks.net/serving-endpoints/credit_card_fraud/invocations'
+    url = 'https://adb-984752964297111.11.azuredatabricks.net/serving-endpoints/bmac_cc_fraud_endpoint/invocations'
     headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}', 'Content-Type': 'application/json'}
     ds_dict = {'dataframe_split': dataset.to_dict(orient='split')} if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
     data_json = json.dumps(ds_dict, allow_nan=True)
@@ -422,7 +436,6 @@ def score_model(dataset):
     if response.status_code != 200:
         raise Exception(f'Request failed with status {response.status_code}, {response.text}')
     return response.json()
-    
 
 data = [
     {
@@ -442,7 +455,7 @@ print(result)
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT ai_query('credit_card_fraud',
+# MAGIC SELECT ai_query('bmac_cc_fraud_endpoint',
 # MAGIC     request =>  named_struct(
 # MAGIC         'primary_account_number', 10048, 
 # MAGIC         'observation_date', '2024-04-15',
